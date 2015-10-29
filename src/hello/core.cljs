667e45570350d1ce4cd376ed32d9dec949f57c20
Justin Barton <bartonjustin@gmail.com>
2015-10-29 22:38:28 +1300

    args

--- src/hello/core.cljs
+++ src/hello/core.cljs
@@ -7,6 +7,7 @@
 
 (defn -main [& args]
   (println "Hello world!")
+  (doseq [arg (.-argv nodejs/process)] (println "arg=" arg))
   (println (.readFileSync fs "Makefile" "utf8")))
 
 (set! *main-cli-fn* -main)
