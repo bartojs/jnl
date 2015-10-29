4baa2cb9133dfa23114705197ff3366d5d0edfe3
Justin Barton <bartonjustin@gmail.com>
2015-10-29 23:07:32 +1300

    aget

--- src/hello/core.cljs
+++ src/hello/core.cljs
@@ -1,13 +1,14 @@
 (ns hello.core
   (:require [cljs.nodejs :as nodejs]))
-
 (nodejs/enable-util-print!)
-
 (def fs (nodejs/require "fs"))
+(def process nodejs/process)
+
+(def home (aget (.-env process) (if (= (.-platform process) "win32") "USERPROFILE" "HOME")))
 
 (defn -main [& args]
   (println "Hello world!")
-  (doseq [arg (.-argv nodejs/process)] (println "arg=" arg))
-  (println (.readFileSync fs "Makefile" "utf8")))
+  (doseq [arg (.-argv process)] (println "arg=" arg))
+  (println (.readFileSync fs (str home "/.jrnl_config") "utf8")))
 
 (set! *main-cli-fn* -main)
