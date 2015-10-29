276430058992eaba365ad493f3dbff0e614db203
Justin Barton <bartonjustin@gmail.com>
2015-10-29 23:15:16 +1300

    read config

--- src/hello/core.cljs
+++ src/hello/core.cljs
@@ -4,11 +4,12 @@
 (def fs (nodejs/require "fs"))
 (def process nodejs/process)
 
-(def home (aget (.-env process) (if (= (.-platform process) "win32") "USERPROFILE" "HOME")))
+(def home (aget process "env" (if (= (.-platform process) "win32") "USERPROFILE" "HOME")))
 
 (defn -main [& args]
   (println "Hello world!")
   (doseq [arg (.-argv process)] (println "arg=" arg))
-  (println (.readFileSync fs (str home "/.jrnl_config") "utf8")))
+  (let [cfg (.parse js/JSON (.readFileSync fs (str home "/.jrnl_config") "utf8"))]
+    (println (.readFileSync fs (aget cfg "journals" "default") "utf8"))))
 
 (set! *main-cli-fn* -main)
